1281795f4d139927aef5c07e2382b61e96dd8027
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-10 21:03:07 +0100

    WIP: setup POST endpoint to add new car data to database

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -5,7 +5,32 @@ const db = require('../data/db-configs.js')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json('api is running')
+  db('cars')
+    .then(cars => {
+      res.json(cars)
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Failed to retrieve cars data from database'
+      })
+    })
+})
+
+router.post('/', (req, res) => {
+  const carData = req.body
+  db('cars')
+    .insert(carData)
+    .then(ids => {
+      db('cars').where({ id: ids[0] })
+      .then(newCarEntry => {
+        res.status(201).json(newCarEntry)
+      })
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Error adding new car to database'
+      })
+    })
 })
 
 module.exports = router
